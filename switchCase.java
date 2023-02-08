07f7675b2b5f2797b7fd24eb9c36c57d8ab05f5d
muhammetDoganci <mhmmtdgn30@gmail.com>
2023-02-08 23:56:43 +0300

    08.02.2023

--- switchCase.java
+++ switchCase.java
@@ -1,11 +1,57 @@
 import java.util.Scanner;
 
+import javax.security.sasl.SaslClient;
+
 public class switchCase {
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
-        System.out.println("Yil giriniz : ");
-        int yil = scan.nextInt();
+        System.out.println("Gun giriniz : ");
+        String gun = scan.nextLine();
+        gun = gun.toLowerCase();
+
+        switch (gun) {
+            case "pazartesi":
+            System.out.println("Hafta ici");
+            break;
+            case "sali":
+            System.out.println("Hafta ici");
+            break;
+            case "carsamba":
+            System.out.println("Hafta ici");
+            break;
+            case "persembe":
+            System.out.println("Hafta ici");
+            break;
+            case "cuma":
+            System.out.println("Hafta ici");
+            break;
+            case "cumartesi":
+            System.out.println("Hafta sonu");
+            break;
+            case "pazar":
+            System.out.println("Hafta sonu");
+            break;
+            default:
+            System.out.println("Gecerli bir gun girin");
+
+        }
+
+        switch (gun){
+            case "pazartesi" :
+            case "sali" :
+            case "carsamba" :
+            case "persembe" :
+            case "cuma" :
+                System.out.println("Hafta ici");
+                break;
+            case "cumartesi" :
+            case "pazar" :
+                System.out.println("Hafta sonu");
+                break;
+            default:
+                System.out.println("Lutfen gecerli bir gun giriniz");
+        }
 
     
     }
